0f94ee036c4415db95a07163f9b92b1b03bd9ac8
Bryce Quintana <50848517+musicmaker773@users.noreply.github.com>
2025-10-25 14:07:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+"""
+# Hello World, Streamlit!
+
+This is a website to demonstrate Streamlit's API.
+You can stop looking at this now.
+
+Please.
+"""
+
+st.write("This is some text made using Python.")
+
+adjective = st.text_input("Type in an adjective")
+noun = st.text_input("Type in a noun")
+verb = st.text_input("Type in a verb in past tense")
+
+st.write("I just gave a " + noun + " twenty " + adjective + " dollars and he " + verb + " me!")
+
+pressed = st.button("Press me!")
+if pressed:
+    st.write("Why did you do that?")
